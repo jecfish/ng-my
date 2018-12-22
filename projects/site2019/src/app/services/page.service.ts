5bbce84e38e87a2ce19348bd9ec982d8a6ee2805
Jecelyn <jecelyn.yeen@randstad.com.my>
2018-12-22 22:23:09 +0800

    simplify seo, change to use page service

---
+++ projects/site2019/src/app/services/page.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Title } from '@angular/platform-browser';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PageService {
+
+  private readonly postfix = ' | ng-MY 2019';
+  private trackingID = environment.gaTrackingID;
+
+  constructor(private title: Title) { }
+
+  setPage(config: { title: string; path: string; skipTitlePostfix?: boolean }) {
+    const postfix = config.skipTitlePostfix ? '' : this.postfix;
+    this.title.setTitle(config.title + postfix);
+
+    if (!environment.production || !window['gtag']) return;
+
+    gtag('config', this.trackingID, {
+      'page-title': config.title,
+      'page-path': config.path
+    });
+  }
+}
