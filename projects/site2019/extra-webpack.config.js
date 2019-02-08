3f660e28637e72298f40e333dbd5c08ff48cf373
chybie <chy.bie@hotmail.com>
2019-02-08 06:07:53 +0000

    add route

--- projects/site2019/extra-webpack.config.js
+++ projects/site2019/extra-webpack.config.js
@@ -20,6 +20,7 @@ module.exports = {
                 // forms
                 ...formRouteList,
                 // team members
+                '/team',
                 ...teamRouteList,
             ],
         })
