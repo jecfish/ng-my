811b6a8efd12c25e788e806d6d1733c68f7edeee
chybie <chy.bie@hotmail.com>
2019-03-04 09:55:38 +0800

    add seo route

--- projects/site2019/extra-webpack.config.js
+++ projects/site2019/extra-webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const PrerenderSPAPlugin = require('prerender-spa-plugin')
 const team = require('./src/assets/data/members.json');
+const speakers = require('./src/assets/data/speakers.json');
 const forms = require('./src/assets/data/forms.json');
 const posts = require('./src/assets/data/posts.json');
 // const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
 
 const teamRouteList = team.map(x => '/team/' + x.id);
+const speakerRouteList = speakers.map(x => '/speakers/' + x.id);
 const formRouteList = Object.keys(forms).map(x => '/form/' + x);
 const postRouteList = Object.keys(posts).map(x => '/post/' + x);
 
@@ -27,6 +29,8 @@ module.exports = {
                 ...teamRouteList,
                 // posts
                 ...postRouteList,
+                '/speakers',
+                ...speakerRouteList,
             ],
         })
     ]
