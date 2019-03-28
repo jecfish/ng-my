73f7294a07b035e8fd93c47c5cdadf20c2d4d0d0
jecelyn <ybc@YBCs-MBP.lan>
2019-03-28 08:11:20 +0200

    disable lazy load img err

--- projects/site2019/src/app/home/home.module.ts
+++ projects/site2019/src/app/home/home.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeTicketPageComponent } from './home-ticket-page/home-ticket-page.component';
-import { DeferLoadModule } from '@trademe/ng-defer-load';
 
 @NgModule({
   declarations: [
@@ -10,8 +9,7 @@ import { DeferLoadModule } from '@trademe/ng-defer-load';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule,
-    DeferLoadModule
+    HomeRoutingModule
   ]
 })
 export class HomeModule { }
