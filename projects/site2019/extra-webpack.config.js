1c2da019945d8f6a8131d3cc653ea8898d3e4d58
jecelyn <ybc@YBCs-MBP.lan>
2019-03-29 07:52:46 +0200

    home

--- projects/site2019/extra-webpack.config.js
+++ projects/site2019/extra-webpack.config.js
@@ -28,6 +28,7 @@ module.exports = {
             // Required - Routes to render.
             routes: [
                 '/', 
+                '/home',
                 '/coc',
                 // posts
                 ...postRouteList,
