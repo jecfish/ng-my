f6b1b4950fdb7a34c34949ccb1fdb7ee5f4693cb
jecelyn <ybc@YBCs-MacBook-Pro.local>
2019-05-13 15:52:10 +0800

    update

--- projects/site2019/src/app/schedule/schedule-page/schedule-page.component.ts
+++ projects/site2019/src/app/schedule/schedule-page/schedule-page.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import schedule from '../../../assets/data/schedule.json';
-import session from '../../../assets/data/sessions.json';
+import speakerList from '../../../assets/data/speakers.json';
+import sessionList from '../../../assets/data/sessions.json';
+import profileIcons from '../../../assets/data/profile-url-icons.json';
 import { PageService } from '../../services/page.service.js';
+import { IModal } from '../../shared/modal/modal.component.js';
 
 @Component({
   selector: 'my-schedule-page',
@@ -15,27 +18,90 @@ export class SchedulePageComponent implements OnInit {
 
   day2 = this.getSchedule(schedule.two);
 
-  constructor(private pageSvc: PageService) {}
+  sessions = [];
+
+  selectedSessionId = null;
+
+  get selectedItem() {
+    const session = this.sessions.find(x => x.id === this.selectedSessionId);
+
+    if (!session) return null;
+
+    const item: IModal = {
+      thumbnails: session.speakers.map(x => {
+        return {
+          url: '',
+          img: `../../assets/imgs/speakers/${x.id}.jpg`,
+          name: x.name,
+          links: x.profile.map(p => {
+            return {
+              url: p.url,
+              type: profileIcons[p.type]
+            };
+          })
+        };
+      }),
+      title: session.title,
+      desc: session.description.full
+    };
+    return item;
+  }
+
+  constructor(private pageSvc: PageService) { }
 
   ngOnInit() {
     this.pageSvc.setPage({
       title: this.title,
       metaDesc: 'Agenda of the days.'
     });
+
+    this.sessions = this.sessions = sessionList
+      .map(x => ({
+        ...x,
+        ...{
+          description: {
+            short: `${x.description.substr(0, 160)}${
+              x.description.length > 160 ? '...' : ''
+              }`,
+            full: x.description
+          }
+        }
+      }))
+      .map(session => {
+        const speakers = speakerList
+          .map(x => ({
+            ...x,
+            ...{
+              food: this.pageSvc.randomFoodIcon()
+            }
+          }))
+          .filter(x => session.speakers.includes(x.id));
+        return { ...session, speakers };
+      })
+      ;
   }
 
   getSchedule(item) {
-    const sessionIds = session.map(x => x.id);
+    const sessionIds = sessionList.map(x => x.id);
     return Object.entries(item).map(([time, items = [] as any]) => ({
       time,
       items: items.map(x =>
         !x.speakers
           ? x
           : {
-              ...x,
-              hasSessionInfo: sessionIds.includes(x.session)
-            }
+            ...x,
+            hasSessionInfo: sessionIds.includes(x.session)
+          }
       )
     }));
+  }
+
+  selectSession(id) {
+    console.log(id);
+    this.selectedSessionId = id;
+  }
+
+  unselectSession() {
+    this.selectedSessionId = null;
   }
 }
--- projects/site2019/src/app/schedule/schedule.module.ts
+++ projects/site2019/src/app/schedule/schedule.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ScheduleRoutingModule } from './schedule-routing.module';
 import { SchedulePageComponent } from './schedule-page/schedule-page.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -9,7 +10,8 @@ import { SchedulePageComponent } from './schedule-page/schedule-page.component';
   ],
   imports: [
     CommonModule,
-    ScheduleRoutingModule
+    ScheduleRoutingModule,
+    SharedModule
   ]
 })
 export class ScheduleModule { }
