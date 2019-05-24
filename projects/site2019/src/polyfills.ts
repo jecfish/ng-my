ba45b7ecdc5d523fa1cb6ec002e79581af67d38e
jecelyn <ybc@ybcs-mbp.lan>
2019-05-24 09:24:11 +0800

    refactor team

--- projects/site2019/src/polyfills.ts
+++ projects/site2019/src/polyfills.ts
@@ -22,24 +22,24 @@
  *  This also includes Android Emulators with older versions of Chrome and Google Search/Googlebot
  */
 
-import 'core-js/es6/symbol';
-import 'core-js/es6/object';
-import 'core-js/es6/function';
-import 'core-js/es6/parse-int';
-import 'core-js/es6/parse-float';
-import 'core-js/es6/number';
-import 'core-js/es6/math';
-import 'core-js/es6/string';
-import 'core-js/es6/date';
-import 'core-js/es6/array';
-import 'core-js/es6/regexp';
-import 'core-js/es6/map';
-import 'core-js/es6/weak-map';
-import 'core-js/es6/set';
+// import 'core-js/es6/symbol';
+// import 'core-js/es6/object';
+// import 'core-js/es6/function';
+// import 'core-js/es6/parse-int';
+// import 'core-js/es6/parse-float';
+// import 'core-js/es6/number';
+// import 'core-js/es6/math';
+// import 'core-js/es6/string';
+// import 'core-js/es6/date';
+// import 'core-js/es6/array';
+// import 'core-js/es6/regexp';
+// import 'core-js/es6/map';
+// import 'core-js/es6/weak-map';
+// import 'core-js/es6/set';
 
-import 'core-js/es7/array';
-import 'core-js/es7/object';
-import 'core-js/es7/string';
+// import 'core-js/es7/array';
+// import 'core-js/es7/object';
+// import 'core-js/es7/string';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
