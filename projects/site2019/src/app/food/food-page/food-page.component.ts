5cba2921f757691519a37dce236bfa6331667bc4
jenning <j3nnning@gmail.com>
2019-06-29 19:22:30 +0800

    update food page design

--- projects/site2019/src/app/food/food-page/food-page.component.ts
+++ projects/site2019/src/app/food/food-page/food-page.component.ts
@@ -9,12 +9,18 @@ import { PageService } from '../../services/page.service';
 export class FoodPageComponent implements OnInit {
   title = 'Food Menu';
 
-  constructor(private pageSvc: PageService) { }
+  day = 1;
+
+  constructor(private pageSvc: PageService) {}
 
   ngOnInit() {
     this.pageSvc.setPage({
       title: this.title,
       metaDesc: 'NG-MY is about the conference and also food!'
     });
+  }
+
+  selectDay(day) {
+    this.day = day;
   }
 }
