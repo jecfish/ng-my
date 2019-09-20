8d9a146336bb5f1b7ced14a38db7eb210b492c10
Jecelyn Yeen <jecelynyeen@Jecelyns-MacBook-Pro.local>
2019-09-20 23:25:58 +0800

    deploy

--- projects/site2019/routes.js
+++ projects/site2019/routes.js
@@ -1,13 +1,13 @@
 const path = require('path');
 const team = require('./src/assets/data/members.json');
 const speakers = require('./src/assets/data/speakers.json');
-const forms = require('./src/assets/data/forms.json');
+// const forms = require('./src/assets/data/forms.json');
 const posts = require('./src/assets/data/posts.json');
 const sessions = require('./src/assets/data/sessions.json');
 
 const teamRouteList = [...team.members.map(x => '/team/' + x.id), ...team.organizers.map(x => '/team/' + x.id)];
 const speakerRouteList = speakers.map(x => '/speakers/' + x.id);
-const formRouteList = Object.keys(forms).map(x => '/form/' + x);
+// const formRouteList = Object.keys(forms).map(x => '/form/' + x);
 const postRouteList = Object.keys(posts).map(x => '/post/' + x);
 const sessionRouteList = sessions.map(x => '/sessions/' + x.id);
 
@@ -23,7 +23,7 @@ const routes = {
         '/sessions',
         ...sessionRouteList,
         '/schedule',
-        ...formRouteList,
+        // ...formRouteList,
         '/team',
         ...teamRouteList,
         '/food',
